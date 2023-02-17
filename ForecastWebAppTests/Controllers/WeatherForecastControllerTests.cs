35d627e9311be6f9e3f6b0c2f41f1d224a9ffb98
Nerijus Medeckis <nerijus.medeckis@lzukt.lt>
2023-02-17 08:29:52 +0200

    Test for WeatherForecastController controller Forecast enpoint

--- ForecastWebAppTests/Controllers/WeatherForecastControllerTests.cs
+++ ForecastWebAppTests/Controllers/WeatherForecastControllerTests.cs
@@ -19,6 +19,44 @@ public class WeatherForecastControllerTests
         new Place() { Code = "1", Name = "" },
     };
 
+    private const string placeCode = "kaunas";
+
+    private Forecast forecast = new Forecast()
+    {
+        ForecastCreationTimeUtc = "2023-02-17 04:20:44",
+        Place = new Place()
+        {
+            Code = "vilnius",
+            Name = "Vilnius",
+            AdministrativeDivision = "Vilniaus miesto savivaldybė",
+            Country = "Lietuva",
+            CountryCode = "LT",
+            Coordinates = new Coordinates()
+            {
+                Latitude = 54.687046,
+                Longitude = 25.282911
+            }
+        },
+         ForecastType = "long-term",
+         ForecastTimestamps = new List<ForecastTimestamp>()
+         {
+             new ForecastTimestamp()
+             {
+                ForecastTimeUtc = "2023-02-17 04:00:00",
+                AirTemperature = 0,
+                FeelsLikeTemperature = -2.4,
+                WindSpeed = 2,
+                WindGust = 4,
+                WindDirection = 179,
+                CloudCover = 100,
+                SeaLevelPressure = 1016,
+                RelativeHumidity = 95,
+                TotalPrecipitation = 0,
+                ConditionCode = "cloudy"
+             }
+         }
+    };
+
     [Test]
     public void Places_ResultFailure_ReturStatusCode500()
     {
@@ -63,6 +101,62 @@ public class WeatherForecastControllerTests
                                           mockService.Object);
 
         var task = controller.Places();
+
+        task.Wait();
+
+        var result = task.Result;
+
+        var okResult = task.Result as OkObjectResult;
+
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That(okResult?.StatusCode, Is.EqualTo(200));
+    }
+
+
+    [Test]
+    public void Forecast_ResultFailure_ReturStatusCode500()
+    {
+        var mockService = new Mock<IForecastService>();
+        mockService.Setup(service => service.GetPlaceForecast(placeCode))
+            .ReturnsAsync(Result.Fail<Forecast>("Error"));
+
+        var mockLogger =
+            new Mock<ILogger<WeatherForecastController>>();
+
+
+        var controller =
+            new WeatherForecastController(mockLogger.Object,
+                                          mockService.Object);
+
+        var task = controller.Forecast(placeCode);
+
+        task.Wait();
+
+        IActionResult actionResult = task.Result;
+
+        var statusCodeResult = actionResult as StatusCodeResult;
+
+        Assert.That(actionResult, Is.InstanceOf<Microsoft.AspNetCore.Mvc.StatusCodeResult>());
+        Assert.That(statusCodeResult, Is.Not.Null);
+        Assert.That(statusCodeResult.StatusCode, Is.EqualTo(500));
+    }
+
+    [Test]
+    public void Forecast_ResultOk_ReturStatusCodeOk()
+    {
+        var mockService = new Mock<IForecastService>();
+        mockService.Setup(service => service.GetPlaceForecast(placeCode))
+            .ReturnsAsync(Result.Success<Forecast>(forecast));
+
+        var mockLogger =
+            new Mock<ILogger<WeatherForecastController>>();
+
+
+        var controller =
+            new WeatherForecastController(mockLogger.Object,
+                                          mockService.Object);
+
+        var task = controller.Forecast(placeCode);
 
         task.Wait();
 
