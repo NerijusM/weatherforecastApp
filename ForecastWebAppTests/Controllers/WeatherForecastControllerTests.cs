f6f6e8b2f4808f80d5dcab696d015e13b390a16a
Nerijus Medeckis <nerijus.medeckis@lzukt.lt>
2023-02-17 08:14:57 +0200

    Tests for WeatherForecastController controller Places endpoint

---
+++ ForecastWebAppTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using ForecastCore.Entities;
+using ForecastCore.Interfaces;
+using ForecastCore.Shared;
+using ForecastWebAppAngular.Controllers;
+using ForecastWebAppAngular.Extensions;
+using ForecastWebAppAngular.ModelsDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+namespace ForecastWebAppTests.Controllers;
+
+[TestFixture]
+public class WeatherForecastControllerTests
+{
+    //private Place place1 = new Place() { Code = "0", Name = "" };
+    //private Place Place2 = new Place() { Code = "1", Name = "" },
+    private IEnumerable<Place> _placesList = new List<Place>()
+    {
+        new Place() { Code = "0", Name = "" },
+        new Place() { Code = "1", Name = "" },
+    };
+
+    //private IEnumerable<PlaceDto> _placesListDto = new List<PlaceDto>()
+    //{
+    //    new Place() { Code = "0", Name = "" }.ToDto(),
+    //    new Place() { Code = "1", Name = "" }.ToDto(),
+    //};
+
+    [Test]
+    public void Places_ResultFailure_ReturStatusCode500()
+    {
+        var mockService = new Mock<IForecastService>();
+        mockService.Setup(service => service.GetPlaces())
+            .ReturnsAsync(Result.Fail<IEnumerable<Place>>("Error"));
+
+        var mockLogger =
+            new Mock<ILogger<WeatherForecastController>>();
+
+
+        var controller =
+            new WeatherForecastController(mockLogger.Object,
+                                          mockService.Object);
+
+        var task = controller.Places();
+
+        task.Wait();
+
+        IActionResult actionResult = task.Result;
+
+        var statusCodeResult = actionResult as StatusCodeResult;
+
+        Assert.That(actionResult, Is.InstanceOf<Microsoft.AspNetCore.Mvc.StatusCodeResult>());
+        Assert.That(statusCodeResult, Is.Not.Null);
+        Assert.That(statusCodeResult.StatusCode, Is.EqualTo(500));
+    }
+
+    [Test]
+    public void Places_ResultOk_ReturStatusCodeOk()
+    {
+        var mockService = new Mock<IForecastService>();
+        mockService.Setup(service => service.GetPlaces())
+            .ReturnsAsync(Result.Success<IEnumerable<Place>>(_placesList));
+
+        var mockLogger =
+            new Mock<ILogger<WeatherForecastController>>();
+
+
+        var controller =
+            new WeatherForecastController(mockLogger.Object,
+                                          mockService.Object);
+
+        var task = controller.Places();
+
+        task.Wait();
+
+        var result = task.Result;
+
+        var okResult = task.Result as OkObjectResult;
+
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That(okResult?.StatusCode, Is.EqualTo(200));
+    }
+}
+
